f611f98ecb9f88cf96ccb680316842bffeec7236
Josiahscott <jossco27@kingshigh.school.nz>
2021-06-18 10:53:59 +1200

    New food campfire

---
+++ Campfire.gd
@@ -0,0 +1,21 @@
+extends Area
+var bodies
+var player = null
+var interact_text = "Press E to Eat"
+
+func _process(delta):
+	if player!= null:
+		if Input.is_action_just_pressed("use"):
+			print("Eat")
+			PlayerStats.change_food(20)
+		
+func _on_Area_body_entered(body):
+	if body.is_in_group("Player"):
+		print("adding player")
+		player = body
+		
+
+func _on_Area_body_exited(body):
+	if body.is_in_group("Player"):
+		print("player leaving")
+		player = null
