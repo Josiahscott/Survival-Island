50ca8e1e864acdcf08bb7ebceebeb296d120ffbc
Josiahscott <jossco27@kingshigh.school.nz>
2021-11-04 10:09:33 +1300

    Update Enemy.gd

--- Enemy.gd
+++ Enemy.gd
@@ -1,18 +1,18 @@
 extends KinematicBody
 #enemy variables
 export var speed = 30
+
 var health = 100 #enemy health
 var shoot = null
 var target = null #target for player is null on start up
 var velocity = Vector3.ZERO
 var shoot_timer : Timer
+
 onready var player = owner.get_node("Player")
-#onready var shoottimer = $ShootTimer
 onready var raycast = $RayCast
 onready var runanim = $Body/Run #loading variables
 onready var spearanim = $Body/Spear
 var gravity = 1 #gravity
-
 
 func _physics_process(delta):
 	
@@ -30,8 +30,6 @@ func _physics_process(delta):
 			PlayerStats.change_health(-5)
 			shoot_timer.start()
 
-
-	
 	var player_distance = translation.distance_to(player.translation)
 	if player_distance < 2.5:
 		target = player #targeting player
